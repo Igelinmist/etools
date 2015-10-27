46fbc5dc43a8fefea27eecc258515b1a195d8e29
Василий Троян < >
2015-10-27 22:32:00 +0600

    Test commit from QUMO

--- etools/apps/uptime/views/journal_views.py
+++ etools/apps/uptime/views/journal_views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 
+from ..models.journal_models import Equipment, Journal
+
 
 def index(request):
-    return render(request, 'uptime/index.html')
+    root = Equipment.objects.filter(plant=None)[0]
+    context = {'equipment_list': root.unit_tree()}
+    return render(request, 'uptime/index.html', context)
+
+
+def show(request, journal_id):
+    journal = get_object_or_404(Journal, journal_id)
+    return render(request, 'uptime/show.html')
